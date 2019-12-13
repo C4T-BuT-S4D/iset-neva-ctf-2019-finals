16a764e24711c489cc446b14d2599a4d34552ffe
rinagert512 <rolage@yandex.ru>
2019-12-13 13:07:27 -0800

    updated web

--- tasks/medium-150/johnson/deploy/index.php
+++ tasks/medium-150/johnson/deploy/index.php
@@ -1,8 +1,5 @@
-<h1> Hello and welcome to our unique website! </h1>
-<h3> It was made to show you example of working in php with files </h3>
-
-
 <?php
+  error_reporting(E_ERROR | E_PARSE);
   class Foo {
     private $path = "info.txt";
    
@@ -13,6 +10,16 @@
 
   $a = new Foo();
   setcookie("some_secret_data", base64_encode(serialize($a)));
+?>
+
+<html>
+<body>
+  <h1> Hello and welcome to our unique website! </h1>
+  <h3> It was made to show you example of working in php with files </h3>
+</body>
+</html>
+
+<?php
   $b = unserialize(base64_decode($_COOKIE["some_secret_data"]));
   $data = $b->show_data();
   $length = strlen($data);
